eb541a157dfa7c93a842735cf22a6610f75f9111
fabriccio27 <fabricio_inc27@hotmail.com>
2021-07-17 10:29:42 -0300

    added some comments, could be shorter using class based views

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    """ Un serializer me permite serializar o deserializar una instancia de un modelo en este caso en algo que puedo comunicar,
+    como un json
+    En Meta le paso el modelo a partir del cual trabajo, y que campos quiero que terminen en el objeto a comunicar """
     class Meta:
         model = Task
         fields = "__all__"
--- api/views.py
+++ api/views.py
@@ -19,6 +19,9 @@ def apiOverview(request):
         "Update":"/task-update/<str:pk>/",
         "Delete":"/task-delete/<str:pk>/"
     }
+    # REST framework also introduces a Response object, which is a type of TemplateResponse that takes unrendered content 
+    # and uses content negotiation to determine the correct content type to return to the client.
+
     return Response(api_urls)
 
 @api_view(["GET"])
@@ -27,12 +30,13 @@ def taskList(request):
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
 
-    #el serializer lo que hace es tomar la abstraccion de models y llevarlo a algo 
-    # que se puede intercambiar facilmente, como un json
+    # el serializer lo que hace es tomar la abstraccion de models y llevarlo a algo 
+    # que se puede intercambiar facilmente, como un json. Tambien aplica validacion a la request.data en base a model
 
 @api_view(["GET"])
 def taskDetail(request, pk):
     tasks = Task.objects.get(id=pk)
+    """ aca podria haber usado get_object_or_404 """
     serializer = TaskSerializer(tasks, many=False)
     return Response(serializer.data)
 
@@ -40,6 +44,8 @@ def taskDetail(request, pk):
 def taskCreate(request):
     # al usar el decorator de api_view para post, me genera una vista con un form para que le ponga content
     # request.data es un json, es un feature de usar este framework 
+    # restframework me da un nuevo objeto request, que simplifica el uso al acceso del dict de info de request
+    # antes tenia que ver si la request era de POST y hacer request.POST
     serializer = TaskSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
